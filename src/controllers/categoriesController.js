d5d049889e96559ab6030288178c897acd734358
Isadora Ávila <isadorgravila@gmail.com>
2022-07-21 15:23:48 -0300

    Inserir categoria

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -11,6 +11,12 @@ export async function getCategories(req, res) {
 }
 
 export async function postCategory(req, res) {
-
-    res.sendStatus(201);
+    const { name } = req.body;
+    try {
+        await connection.query('INSERT INTO categories (name) VALUES ($1);', [name]);
+        res.sendStatus(201);
+        
+    } catch (error) {
+        res.status(500).send(error);
+    }
 }
--- src/middlewares/categoriesValidation.js
+++ src/middlewares/categoriesValidation.js
@@ -1,14 +1,24 @@
 import categoriesSchema from '../schemas/categoriesSchema.js';
+import connection from "../databases/postgres.js";
 
-async function categoriesValidation (req, res, next) {
+async function categoriesValidation(req, res, next) {
     const validation = categoriesSchema.validate(req.body);
 
     if (validation.error) {
-        
         return res.sendStatus(400);
     }
 
-    //validar se categoria já existe no banco de dados
+    const { name } = req.body;
+    
+    try {
+        const { rows: repeated } = await connection.query('SELECT * FROM categories WHERE name = $1', [name]);
+        if (repeated.length > 0) {
+            return res.sendStatus(409);
+        }
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
 
     next();
 }
