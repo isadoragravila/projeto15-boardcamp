af9a59eaf7f095e16d47ce7ac1702d7d81800ce2
Isadora Ávila <isadorgravila@gmail.com>
2022-07-21 15:02:08 -0300

    Listar categorias

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,9 +1,16 @@
-export async function getCategories (req, res) {
-    
-    res.status(200).send('categories');
+import connection from "../databases/postgres.js";
+
+export async function getCategories(req, res) {
+    try {
+        const { rows: categories } = await connection.query('SELECT * FROM categories');
+        res.status(200).send(categories);
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
 }
 
-export async function postCategory (req, res) {
-    
+export async function postCategory(req, res) {
+
     res.sendStatus(201);
 }
