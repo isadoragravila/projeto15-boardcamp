354ee4465922b4428f5bb77aab179816b866de30
Isadora Ávila <isadorgravila@gmail.com>
2022-07-22 14:11:37 -0300

    Inserir aluguel + validações

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -1,16 +1,32 @@
+import connection from "../databases/postgres.js";
+import dayjs from "dayjs";
 
 export async function getRentals (req, res) {
     res.status(200).send('get rentals');
 }
 
-export async function postRental (req, res) {
-    res.sendStatus(201);
+export async function postRental(req, res) {
+    const { customerId, gameId, daysRented } = req.body;
+    const rentDate = dayjs().format('YYYY-MM-DD');
+    const price = res.locals.price;
+
+    try {
+        await connection.query(`
+        INSERT INTO rentals
+        ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+            [customerId, gameId, rentDate, daysRented, null, daysRented * price , null]
+        );
+        res.sendStatus(201);
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
 }
 
-export async function finishRental (req, res) {
+export async function finishRental(req, res) {
     res.sendStatus(200);
 }
 
-export async function deleteRental (req, res) {
+export async function deleteRental(req, res) {
     res.sendStatus(200);
 }
--- src/middlewares/rentalsValidation.js
+++ src/middlewares/rentalsValidation.js
@@ -1,16 +1,28 @@
 import rentalsSchema from '../schemas/rentalsSchema.js';
+import connection from "../databases/postgres.js";
 
-async function rentalsValidation (req, res, next) {
+async function rentalsValidation(req, res, next) {
     const validation = rentalsSchema.validate(req.body);
 
     if (validation.error) {
-        
         return res.sendStatus(400);
     }
 
-    //validar se customerId existe no banco de dados, se não - 400
-    //validar se gameId existe no banco de dados, se não - 400
-    //verificar se existem jogos disponíveis, não ter alugueis em aberto acima da quantidade de jogos em estoque, se não - 400
+    const { customerId, gameId } = req.body;
+    const { rows: customer } = await connection.query('SELECT * FROM customers WHERE id = $1', [customerId]);
+    const { rows: game } = await connection.query('SELECT * FROM games WHERE id = $1', [gameId]);
+
+    if (customer.length === 0 || game.length === 0) {
+        return res.sendStatus(400);
+    }
+
+    const { rows: gameRentals } = await connection.query('SELECT * FROM rentals WHERE "gameId" = $1 AND "returnDate" IS NULL', [gameId]);
+
+    if (game[0].stockTotal < gameRentals.length) {
+        return res.sendStatus(400);
+    }
+
+    res.locals.price = game[0].pricePerDay;
 
     next();
 }
