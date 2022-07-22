2059556ff5cf3264ef6009a65635835bf452e44c
Isadora Ávila <isadorgravila@gmail.com>
2022-07-21 22:57:16 -0300

    Listar jogos e inserir jogo

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -1,8 +1,42 @@
+import connection from "../databases/postgres.js";
 
-export async function getGames (req, res) {
-    res.status(200).send('get games');
+export async function getGames(req, res) {
+    try {
+        const { name } = req.query;
+
+        if (name) {
+            const { rows: games } = await connection.query(`
+            SELECT games.*, categories.name as "categoryName" FROM games
+            JOIN categories
+            ON games."categoryId" = categories.id
+            WHERE games.name LIKE $1
+            `, [`${name}%`]);
+            return res.status(200).send(games);
+        }
+
+        const { rows: games } = await connection.query(`
+        SELECT games.*, categories.name as "categoryName" FROM games
+        JOIN categories
+        ON games."categoryId" = categories.id
+    `);
+        return res.status(200).send(games);
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
 }
 
-export async function postGame (req, res) {
-    res.sendStatus(201);
+export async function postGame(req, res) {
+    const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+    try {
+        await connection.query(`
+        INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") 
+        VALUES ($1, $2, $3, $4, $5);`,
+            [name, image, stockTotal, categoryId, pricePerDay]
+        );
+        res.sendStatus(201);
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
 }
--- src/middlewares/gamesValidation.js
+++ src/middlewares/gamesValidation.js
@@ -1,15 +1,28 @@
 import gamesSchema from '../schemas/gamesSchema.js';
+import connection from "../databases/postgres.js";
 
-async function gamesValidation (req, res, next) {
+async function gamesValidation(req, res, next) {
     const validation = gamesSchema.validate(req.body);
 
     if (validation.error) {
-        
         return res.sendStatus(400);
     }
 
-    //validar se categoriaId existe no banco de dados, se não - 400
-    //validar se name existe no banco de dados, se sim - 409
+    const { name, categoryId } = req.body;
+
+    try {
+        const { rows: repeated } = await connection.query('SELECT * FROM games WHERE name = $1', [name]);
+        if (repeated.length > 0) {
+            return res.sendStatus(409);
+        }
+
+        const { rows: category } = await connection.query('SELECT * FROM categories WHERE id = $1', [categoryId]);
+        if (category.length === 0) {
+            return res.sendStatus(400);
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
 
     next();
 }
