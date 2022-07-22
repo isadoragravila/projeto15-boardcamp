40460710e3b996b533995105ae74e489005b2362
Isadora Ávila <isadorgravila@gmail.com>
2022-07-21 23:30:50 -0300

    Inserir cliente

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -32,7 +32,18 @@ export async function getCustomerById(req, res) {
 }
 
 export async function postCustomer(req, res) {
-    res.sendStatus(201);
+    const { name, phone, cpf, birthday } = req.body;
+    try {
+        await connection.query(`
+        INSERT INTO customers (name, phone, cpf, birthday) 
+        VALUES ($1, $2, $3, $4)`,
+            [name, phone, cpf, birthday]
+        );
+        res.sendStatus(201);
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
 }
 
 export async function updateCustomer(req, res) {
--- src/middlewares/customersValidation.js
+++ src/middlewares/customersValidation.js
@@ -1,14 +1,22 @@
 import customersSchema from '../schemas/customersSchema.js';
+import connection from "../databases/postgres.js";
 
 async function customersValidation (req, res, next) {
     const validation = customersSchema.validate(req.body);
 
     if (validation.error) {
-        
         return res.sendStatus(400);
     }
 
-    //validar se cpf existe no banco de dados, se sim - 409
+    const { cpf } = req.body;
+    try {
+        const { rows: repeatedCpf } = await connection.query('SELECT * FROM customers WHERE cpf = $1', [cpf]);
+        if (repeatedCpf.length > 0) {
+            return res.sendStatus(409);
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
 
     next();
 }
