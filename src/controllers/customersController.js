dbb5f26c75bfbad622334fba1533ce2451fe1663
Isadora Ávila <isadorgravila@gmail.com>
2022-07-21 23:46:38 -0300

    Atualizar dados do cliente

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -47,5 +47,19 @@ export async function postCustomer(req, res) {
 }
 
 export async function updateCustomer(req, res) {
-    res.sendStatus(200);
+    const { id } = req.params;
+    const { name, phone, cpf, birthday } = req.body;
+
+    try {
+        await connection.query(`
+        UPDATE customers
+        SET name = $1, phone = $2, cpf = $3, birthday = $4
+        WHERE id = $5`,
+            [name, phone, cpf, birthday, id]
+        );
+        res.sendStatus(200);
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
 }
