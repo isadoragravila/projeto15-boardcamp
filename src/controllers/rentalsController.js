18d3997aebf8a7f8b19d41a580194f1cd6ede36b
Isadora Ávila <isadorgravila@gmail.com>
2022-07-22 17:04:24 -0300

    Listar alugueis + query strings de listar alugueis

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -2,7 +2,44 @@ import connection from "../databases/postgres.js";
 import dayjs from "dayjs";
 
 export async function getRentals(req, res) {
-    res.status(200).send('get rentals');
+    try {
+        const { customerId, gameId } = req.query;
+        let rentals = [];
+
+        const select = `SELECT rentals.*, customers.name AS "customerName", games.name AS "gameName", games."categoryId", categories.name AS "categoryName" FROM rentals JOIN customers ON rentals."customerId" = customers.id JOIN games ON rentals."gameId" = games.id JOIN categories ON games."categoryId" = categories.id`;
+
+        if (customerId) {
+            const { rows } = await connection.query(`${select} WHERE "customerId" = $1`, [customerId]);
+            rentals = [ ...rows ];
+        } else if (gameId) {
+            const { rows } = await connection.query(`${select} WHERE "gameId" = $1`, [gameId]);
+            rentals = [ ...rows ];
+        } else {
+            const { rows } = await connection.query(`${select}`);
+            rentals = [ ...rows ];
+        }
+
+        rentals.map(rental => {
+            rental.customer = {
+                id: rental.customerId,
+                name: rental.customerName
+            };
+            rental.game = {
+                id: rental.gameId,
+                name: rental.gameName,
+                categoryId: rental.categoryId,
+                categoryName: rental.categoryName
+            }
+            delete rental.customerName;
+            delete rental.gameName;
+            delete rental.categoryId;
+            delete rental.categoryName;
+        })
+        res.status(200).send(rentals);
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
 }
 
 export async function postRental(req, res) {
